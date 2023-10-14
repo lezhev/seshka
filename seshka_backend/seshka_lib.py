571899d9a63456f8e526fe4c8e1c135628be5c5e
Maksim Levchenko <valvekat@mail.ru>
2023-10-14 14:48:52 +0300

    feat: Created set method
    Created set method for Seller and tests for it. Repaired pathes to files

--- seshka_backend/seshka_lib.py
+++ seshka_backend/seshka_lib.py
@@ -1,12 +1,13 @@
 import pandas as pd
 from typing import TypeVar
 from dataclasses import dataclass
+import os
 
 
-items_db: pd.DataFrame = pd.read_feather('databases/items.feather')
-seller_sub_db: pd.DataFrame = pd.read_feather('databases/seller_subscription.feather')
-favorites_db: pd.DataFrame = pd.read_feather('databases/buyers_favorite.feather')
-buyer_sub_db: pd.DataFrame = pd.read_feather('databases/buyers_subscription.feather')
+
+# seller_sub_db: pd.DataFrame = pd.read_feather('databases/seller_subscription.feather')
+# favorites_db: pd.DataFrame = pd.read_feather('databases/buyers_favorite.feather')
+# buyer_sub_db: pd.DataFrame = pd.read_feather('databases/buyers_subscription.feather')
 
 @dataclass
 class Item:
@@ -20,14 +21,16 @@ class Item:
     ad_date: str
 
 class Seller:
-    def __new__(cls, chat_id: int):
-        pass
-
-    def __init__(self, chat_id: int):
-        pass
-
-    def set_item(self, item: Item):
-        pass
+    @staticmethod
+    def set_item(item: Item) -> None:
+        directory = os.path.dirname(os.path.abspath(__file__))
+        items_db: pd.DataFrame = pd.read_feather(directory + '\databases\items.feather')
+        items_db.loc[len(items_db.index) + 1] = [item.photo, item.seller_id,
+                           item.title, item.description,
+                           item.size, item.price,
+                           item.tags, item.ad_date]
+        items_db.to_feather(directory + '\databases\items.feather')
+        print(items_db)
 
 class Buyer:
     def __new__(cls, chat_id: int):
--- seshka_backend/tests/seshka_lib_test.py
+++ seshka_backend/tests/seshka_lib_test.py
@@ -1,10 +1,12 @@
 import unittest
 import pandas as pd
-from seshka_backend.seshka_lib import Seller, Buyer
+from seshka_backend.seshka_lib import Seller, Buyer, Item
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        pass
+    def test_set_item(self) -> None:
+        item: Item = Item(1, 1, 'a', 'b', 'c', 2, [0, 0], '123')
+        Seller.set_item(item)
+
 
 
 if __name__ == '__main__':
