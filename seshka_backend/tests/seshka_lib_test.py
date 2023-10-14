7792d39c5e5ab6f746a23e45cc148a578db2526f
Maksim Levchenko <valvekat@mail.ru>
2023-10-14 12:43:35 +0300

    test: Created tests template
    Created empty tests file

---
+++ seshka_backend/tests/seshka_lib_test.py
@@ -0,0 +1,11 @@
+import unittest
+import pandas as pd
+from seshka_backend.seshka_lib import Seller, Buyer
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
