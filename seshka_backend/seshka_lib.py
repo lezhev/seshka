8d21445bc559b68145ccdd4803938f9ea1affcc2
Maksim Levchenko <valvekat@mail.ru>
2023-10-14 19:07:46 +0300

    Created get_item method

--- seshka_backend/seshka_lib.py
+++ seshka_backend/seshka_lib.py
@@ -2,7 +2,7 @@ import pandas as pd
 from typing import TypeVar
 from dataclasses import dataclass
 import os
-
+import datetime
 
 
 # seller_sub_db: pd.DataFrame = pd.read_feather('databases/seller_subscription.feather')
@@ -17,41 +17,47 @@ import os
 @dataclass
 class Item:
     photo: str
-    seller_id: int
     title: str
     description: str
     size: str
     price: int
     tags: dict
-    ad_date: str
 
-    def __str__(self):
+    def __str__(self) -> str:
         tag_list = []
         for key in self.tags.keys():
             if self.tags[key] == 1:
                 tag_list.append(key)
-        result = (f'# {self.title}\nРазмер: {self.size}\n'
-                  f'{self.description}\n\n'
-                  f'_{self.price}_\n{tag_list}')
+        tag_str: str = ', '.join(tag_list)
+        result = (f'*Имя: {self.title}*\nРазмер: {self.size}\n'
+                  f'Описание: {self.description}\n\n'
+                  f'Цена: _{self.price}_\nТэги: {tag_str}')
         return result
 
 class Seller:
     #Adding item to items.feather
     @staticmethod
-    def set_item(item: Item) -> None:
+    def set_item(seller_id: int, item: Item) -> None:
         directory = os.path.dirname(os.path.abspath(__file__))
         items_db: pd.DataFrame = pd.read_feather(directory + '\databases\items.feather')
-        print(items_db)
-        items_db.loc[len(items_db.index) + 1] = [item.photo, item.seller_id,
+        current_date = datetime.date.today().isoformat()
+        items_db.loc[len(items_db.index) + 1] = [item.photo, seller_id,
                            item.title, item.description,
                            item.size, item.price,
-                           item.tags, item.ad_date]
+                           item.tags, current_date]
         items_db.to_feather(directory + '\databases\items.feather')
-        print(items_db)
+        #print(items_db)
 
     @staticmethod
-    def get_item_from_index(item_index: int) -> str:
-        pass
+    def get_item_text(index: int) -> str:
+        directory = os.path.dirname(os.path.abspath(__file__))
+        items_db: pd.DataFrame = pd.read_feather(directory + '\databases\items.feather')
+        item_list = items_db.iloc[index]
+        print(item_list)
+        item: Item =  Item(title=item_list.title, description=item_list.description,
+                           photo=item_list.pic, size=item_list.size,
+                           price=item_list.price, tags=item_list.tags)
+        return item
 
 
 class Buyer:
--- seshka_backend/tests/seshka_lib_test.py
+++ seshka_backend/tests/seshka_lib_test.py
@@ -4,13 +4,16 @@ from seshka_backend.seshka_lib import Seller, Buyer, Item
 
 class SellerTest(unittest.TestCase):
     def test_set_item(self) -> None:
-        item: Item = Item('1', 1, 'a', 'b', 'c', 2, {'disco': 1}, '123')
-        Seller.set_item(item)
+        item: Item = Item('1', 'a', 'b', 'c', 2, {'disco': 1, 'zombie': 1, 'game': 0})
+        Seller.set_item(1, item)
+
+    def test_get_item_text(self) -> None:
+        print(Seller.get_item_text(3))
 
 class ItemTest(unittest.TestCase):
     def test_str(self) -> None:
-        item: Item = Item('1', 1, 'a', 'b', 'c', 2, {'disco': 1}, '123')
-        print(item)
+        item: Item = Item('1', 'a', 'b', 'c', 2, {'disco': 1, 'zombie': 1, 'game': 0})
+        #print(item)
 
 
 
